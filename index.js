712f28f8e7fc3afbd91b9dc7333467f564be6991
Dorgalam <dorgalam@gmail.com>
2017-01-05 22:40:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var parseString = require('xml2js').parseString;
+
+app.get('/rss', function(req, res) {
+  var sessionId;
+  var options = {
+    'method': 'GET',
+    'url': 'http://192.168.1.1:9091/transmission/rpc',
+    'auth': {
+      'user': 'root',
+      'pass': 'Ducktales1!',
+      'sendImmediately': false
+    }
+  };
+  request.get('http://192.168.1.1:9091/transmission/rpc', options,
+  function(request, response, body) {
+    options.headers = {};
+    options.headers["X-Transmission-Session-Id"] = response.headers["x-transmission-session-id"];
+    addTorrent();
+  });
+  var addTorrent = function() {
+    options.method = 'POST';
+    options.json = {
+      "arguments": {
+        "filename": ""
+      },
+      "method": "torrent-add"
+    }
+    request("http://showrss.info/user/117264.rss?magnets=true&namespaces=true&name=null&quality=null&re=null", function(zibi, response, body) {
+      parseString(body, function(err, result) {
+        result.rss.channel[0].item.forEach(function(item){
+          if((new Date() - new Date(item.pubDate))/(3600*24*1000) < 1) {
+            options.json.arguments["download-dir"] = "/mnt/sda4/Media/Series/" + item["tv:show_name"][0];
+            options.json.arguments.filename = item.link[0];
+            request(options);
+          }
+        })
+      });
+    })
+  }
+})
+
+
+app.listen(3000);
